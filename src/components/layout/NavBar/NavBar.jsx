52ee7b4aed982dcf8d8e36cc29471e2813322882
Gabriel Franco <gabriel.silva.12@aluno.cefet-rj.br>
2022-10-28 11:56:58 -0300

    Implementação do React e criação dos componentes de layout

---
+++ src/components/layout/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import styles from './NavBar.module.css'
+
+export default function NavBar(){
+    return(
+        <nav className={styles.nav_container}>
+            <ul>
+                <li>
+                    Carrinho
+                </li>
+                <li>
+                    Meus pedidos
+                </li>
+                <li>
+                    Login
+                </li>
+                <li>
+                    Cadastrar
+                </li>
+            </ul>
+        </nav>
+    )
+}
