fcd45ecdf058d83df29d39fd419e14864eed5f9c
Gabriel Franco <gabriel.silva.12@aluno.cefet-rj.br>
2022-11-06 22:19:51 -0300

    Mudanças no botão

--- src/components/layout/Button/Button.jsx
+++ src/components/layout/Button/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css'
 
-export default function Button({ conteudoBtn }) {
+export default function Button({ conteudoBtn}) {
 
     return (
         <button className={styles.button_area}>{conteudoBtn}</button>
