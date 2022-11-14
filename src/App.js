6b67077fa18d64aff8782bad11e086c3418f36d4
Gabriel Franco <gabriel.silva.12@aluno.cefet-rj.br>
2022-11-13 22:45:10 -0300

    Implementação do banco mockado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import Header from './components/layout/Header/Header'
 import Footer from './components/layout/Footer/Footer';
 import Home from './components/pages/Home/Home';
@@ -8,19 +7,12 @@ import NovoProduto from './components/pages/NovoProduto/NovoProduto';
 
 function App() {
 
-  let [data, setData] = useState({})
-
-  function recebeDados(DATA) {
-    setData(DATA)
-  }
-
-
   return (
     <Router>
       <Header />
       <Routes>
-        <Route exact path='/' element={<Home sobeDados={recebeDados} />} />
-        <Route path='/product/:id' element={<Produto data={data} />} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Produto/>} />
         <Route path='/new-product' element={<NovoProduto />} />
       </Routes>
       <Footer />
