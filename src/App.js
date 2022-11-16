59cd62c386095bf203c9207dca4f1bcecf2d26a4
João Victor Reis <jv.rreis22@gmail.com>
2022-11-16 09:18:40 -0300

    Mudanças

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Footer from './components/layout/Footer/Footer';
 import Home from './components/pages/Home/Home';
 import Produto from './components/pages/Produto/Produto';
 import NovoProduto from './components/pages/NovoProduto/NovoProduto';
+import Chat from './components/pages/Chat/Chat';
+import "./style.scss";
+
+
 
 function App() {
 
@@ -14,6 +18,7 @@ function App() {
         <Route exact path='/' element={<Home />} />
         <Route path='/product/:id' element={<Produto/>} />
         <Route path='/new-product' element={<NovoProduto />} />
+        <Route path='/Chat' element={<Chat />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/pages/Chat/Chat.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import Messages from "./Messages";
+
+
+const Chat = () => {
+
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>'Username'</span>
+        <div className="chatIcons">
+        </div>
+      </div>
+      <Messages />
+    </div>
+  );
+};
+
+export default Chat;
