0a9f11a2c1c597eaa80ddafc56661e7ad1212767
Gabriel Franco <gabriel.silva.12@aluno.cefet-rj.br>
2022-11-24 16:48:54 -0300

    Inicio do sistema de login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -2,17 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-import {useForm} from 'react-hook-form'
-
+import { useForm } from 'react-hook-form'
+import { users } from '../../../atoms'
 import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
-export default function Login()
-{
+export default function Login() {
 
-    const { register, handleSubmit, formState: { errors }, reset, trigger,} = useForm()
+    const { register, handleSubmit, formState: { errors }, reset, trigger, } = useForm()
+
+    const usersList = useRecoilValue(users)
 
     const onSubmit = (data) => {
-        console.log(data)
+        const userExist = usersList.filter((user) => {
+            return user.email === data.usuario
+        })
+        const doesUserExist = userExist.length === 0 ? false : true
+        console.log(doesUserExist)
         reset()
     }
 
@@ -20,50 +26,50 @@ export default function Login()
         console.log(errors)
     }
 
-    return(
+    return (
 
         <>
-          <Container className='d-flex justify-content-center align-items-end mb-5 mt-5'>
+            <Container className='d-flex justify-content-center align-items-end mb-5 mt-5'>
 
-            <Form className='border border-dark p-5 rounded bg-dark text-white' onSubmit={handleSubmit(onSubmit, onError)}>
+                <Form className='border border-dark p-5 rounded bg-dark text-white' onSubmit={handleSubmit(onSubmit, onError)}>
 
-                <h1 className='text-primary'>Bem vindo ao Collecting</h1>
-                <Form.Group>
+                    <h1 className='text-primary'>Bem vindo ao Collecting</h1>
+                    <Form.Group>
 
-                    <Form.Label className='mt-5'>Usuário</Form.Label>
-                    <Form.Control type='text' placeholder='Digite seu nome' {...register("usuario", {required: "Por favor, digite seu nome"})}
-                            onKeyUp={() =>{
+                        <Form.Label className='mt-5'>Usuário</Form.Label>
+                        <Form.Control type='text' placeholder='Digite seu nome' {...register("usuario", { required: "Por favor, digite seu nome" })}
+                            onKeyUp={() => {
                                 trigger("usuario")
                             }}></Form.Control>
-                    {errors.usuario && (<small className="text-danger">{errors.usuario.message}</small>)}
-                </Form.Group>
+                        {errors.usuario && (<small className="text-danger">{errors.usuario.message}</small>)}
+                    </Form.Group>
 
-                <Form.Group>
+                    <Form.Group>
 
-                    <Form.Label className='mt-5'>Senha</Form.Label>
-                    <Form.Control type='password' placeholder='Digite sua senha' {...register("senha", {required: "Por favor, informe sua senha"})}
-                             onKeyUp={() =>{
+                        <Form.Label className='mt-5'>Senha</Form.Label>
+                        <Form.Control type='password' placeholder='Digite sua senha' {...register("senha", { required: "Por favor, informe sua senha" })}
+                            onKeyUp={() => {
                                 trigger("senha")
                             }}></Form.Control>
-                    {errors.senha && (<small className="text-danger">{errors.senha.message}</small>)}
-                </Form.Group>
+                        {errors.senha && (<small className="text-danger">{errors.senha.message}</small>)}
+                    </Form.Group>
 
-                <Form.Group>
+                    <Form.Group>
 
-                    <Form.Control className='btn btn-primary mt-5' type='submit' value='Login'></Form.Control>
+                        <Form.Control className='btn btn-primary mt-5' type='submit' value='Login'></Form.Control>
 
-                </Form.Group>
+                    </Form.Group>
 
-                 <Form.Group  className='d-flex mt-3'>
+                    <Form.Group className='d-flex mt-3'>
 
-                    <p className='mt-3'>Não tem conta?</p>
-                    <Link to={'/sign-up'} className='mt-3 ms-3 text-decoration-none'>Cadastre-se</Link>
+                        <p className='mt-3'>Não tem conta?</p>
+                        <Link to={'/sign-up'} className='mt-3 ms-3 text-decoration-none'>Cadastre-se</Link>
 
-                </Form.Group>
-                 
-            </Form>
+                    </Form.Group>
 
-          </Container>
+                </Form>
+
+            </Container>
         </>
     )
 }
